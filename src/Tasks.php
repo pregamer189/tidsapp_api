e1aabbbb248f9b723e4673d873cc9cb707f712be
Elias Blomberg <blomberg189@gmail.com>
2024-01-24 13:49:35 +0200

    Spara ny post funkar, inga tester klara

--- src/Tasks.php
+++ src/Tasks.php
@@ -204,6 +204,39 @@ function hamtaEnskildUppgift(string $id): Response {
  */
 function sparaNyUppgift(array $postData): Response {
     
+    // Kontrollera indata
+    $felmeddelande = kontrolleraIndata($postData);
+
+    if (count($felmeddelande) > 0) {
+        $retur = new stdClass();
+        $retur->error = $felmeddelande;
+        array_unshift($retur->error, 'Bad request');
+        return new Response($retur, 400);
+    }
+
+    // Koppla mot databas
+    $db = connectDb();
+
+    // Exekvera databasfråga
+    $stmt = $db->prepare("INSERT INTO uppgifter (datum, tid, beskrivning, aktivitetId) "
+            . "VALUES (:datum, :tid, :beskrivning, :aktivitetId)");
+    $stmt->execute(['datum'=>$postData["date"], 'tid'=>$postData["time"], 
+                    'beskrivning'=>trim(filter_var($postData["description"], FILTER_SANITIZE_SPECIAL_CHARS)),
+                    'aktivitetId'=>$postData["activityId"]]);
+
+    // Kontrollera svar
+    if ($stmt->rowCount() === 1) {
+        $retur = new stdClass();
+        $retur->id=$db->lastInsertId(); 
+        $retur->message=['Skapa ny post lyckades', '1 post sparad'];
+        return new Response($retur);
+
+    } else {
+        $retur = new stdClass();
+        $retur->error = ["Fel vid databasanrop", "Kunde inte skapa post"];
+        return new Response($retur, 400);
+
+    }
 }
 
 /**
--- src/funktioner.php
+++ src/funktioner.php
@@ -18,3 +18,49 @@ function connectDb(): PDO {
 
     return $db;
 }
+
+function kontrolleraIndata(array $postdata):array {
+    $retur = [];
+
+    // Kontrollera datum $postdata["date"]
+    $datum = DateTimeImmutable::createFromFormat("Y-m-d", $postdata['date']??'');
+    if (!$datum) {
+        $retur[] = "Ogiltigt angivet datum";
+    }
+
+    if ($datum && $datum->format("Y-m-d") !== $postdata["date"]) {
+        $retur[] = "Felaktigt formaterat datum";
+    } 
+
+    if ($datum && $datum->format("Y-m-d") > date("Y-m-d")) {
+        $retur[] = "Datumet får inte vara framåt i tiden";
+    }
+
+    // Kontrollera tid $postdata["time"]
+    $tid = DateTimeImmutable::createFromFormat("H:i", $postdata["time"]??'');
+    if (!$tid) {
+        $retur[] = "Ogiltigt angiven tid";
+    }
+
+    if ($tid && $tid->format("H:i") !== $postdata["time"]) {
+        $retur[] = "Felaktigt angiven tid";
+    }
+
+    if ($tid && $tid->format("H:i") > "08:00") {
+        $retur[] = "Du får inte raportera mer än 8 timmar per aktivitet åt gången";
+    }
+
+    // Kontrollera aktivitetsId $postdata["activityId"]
+    $aktivitet = hamtaEnskildAktivitet($postdata["activityId"]??'');
+    if ($aktivitet->getStatus() === 400) {
+        $retur[] = "Angivet aktivitets id saknas";
+    }
+
+    // Ta bort konstiga tecken från beskrivning $postdata["description"]
+    if (trim(filter_var($postdata["description"], FILTER_SANITIZE_SPECIAL_CHARS)) === "") {
+        $retur[] = "Beskrivning saknas";
+    }
+
+
+    return $retur;
+}
