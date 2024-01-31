aaa4d2aa311149cdb33c793bc94e668d50e3b255
Elias Blomberg <blomberg189@gmail.com>
2024-01-31 12:59:20 +0200

    Hämta Sammanställning och tester funkar

--- src/Compilations.php
+++ src/Compilations.php
@@ -27,4 +27,70 @@ function compilations(Route $route): Response {
  * @return Response
  */
 function hamtaSammanstallning(string $from, string $tom): Response {
+
+    // Kontrollera indata
+
+    $fromDate = DateTimeImmutable::createFromFormat("Y-m-d", $from);
+    $tomDate = DateTimeImmutable::createFromFormat("Y-m-d", $tom);
+    $datumFel = [];
+
+    if ($fromDate === false) {
+        $datumFel[] = "Ogiltigt från-datum";
+    }
+
+    if ($tomDate === false) {
+        $datumFel[] = "Ogiltigt till-datum";
+    }
+
+
+    if($fromDate && $fromDate->format("Y-m-d")!==$from){
+        $datumFel[] = "Ogiltigt angivet från-datum";
+    }
+
+    if($tomDate && $tomDate->format("Y-m-d")!==$tom){
+        $datumFel[] = "Ogiltigt angivet till-datum";
+    }
+
+
+    if ($fromDate && $tomDate && $fromDate->format("Y-m-d") > $tomDate->format("Y-m-d")) {
+        $datumFel[] = "Från-datum får inte vara större än till-datum";
+    }
+
+
+    if (count($datumFel) > 0) {
+        $retur = new stdClass();
+        $retur->error = $datumFel;
+        array_unshift($retur->error, 'bad request');
+        return new Response($retur, 400);
+    }
+
+    // Koppla databas
+    $db = connectDb();
+
+    // Exekvera databasfråga
+    $stmt = $db->prepare("SELECT aktivitetid, SEC_TO_TIME(SUM(TIME_TO_SEC(tid))) AS tid, namn AS aktivitet "
+            . "FROM uppgifter u INNER JOIN aktiviteter a ON u.aktivitetid = a.id "
+            . "WHERE datum BETWEEN :from AND :to "
+            . "GROUP BY aktivitetid");
+    $stmt->execute([
+        ":from" => $fromDate->format("Y-m-d"),
+        ":to" => $tomDate->format("Y-m-d")
+    ]);
+
+
+    // Returnera svar
+    $poster=[];
+    while ($row = $stmt->fetch()) {
+        $rad = new stdClass();
+        $rad->activityId = $row["aktivitetid"];
+        $rad->tid=substr($row["tid"], 0,-3);
+        $rad->activity=$row["aktivitet"];
+        $poster[]=$rad;
+    }
+    $retur = new stdClass();
+    $retur->tasks = $poster;
+
+    return new Response($retur);
+
+
 }
--- test/TestCompilation.php
+++ test/TestCompilation.php
@@ -21,7 +21,59 @@ function allaCompilationTester(): string {
 function test_HamtaSammanstallning(): string {
     $retur = "<h2>test_HamtaSammanstallning</h2>";
     try {
-        $retur .= "<p class='error'>Inga tester implementerade</p>";
+
+        // Misslyckas med ifrån=2024-12-37 till=2024-01-01
+        $svar = hamtaSammanstallning('2024-12-37', '2024-01-01');
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Misslyckades med att hämta poster mellan 2024-12-37 och 2024-01-01, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Misslyckat test med att hämta poster mellan 2024-12-37 och 2024-01-01<br>"
+                    . $svar->getStatus() . " Returnerades istället för förväntat 400</p>";
+        }
+
+        // Misslyckas med ifrån=2024-01-01 till=2024-12-37
+        $svar = hamtaSammanstallning('2024-01-01', '2024-12-37');
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Misslyckades med att hämta poster mellan 2024-01-01 och 2024-12-37, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Misslyckat test med att hämta poster mellan 2024-01-01 och 2024-12-37<br>"
+                    . $svar->getStatus() . " Returnerades istället för förväntat 400</p>";
+        }
+
+        // Misslyckas med ifrån=2024-01-01 till=2023-01-01
+        $svar = hamtaSammanstallning('2024-01-01', '2023-01-01');
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Misslyckades med att hämta poster mellan 2024-01-01 och 2023-01-01, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Misslyckat test med att hämta poster mellan 2024-01-01 och 2023-01-01<br>"
+                    . $svar->getStatus() . " Returnerades istället för förväntat 400</p>";
+        }
+
+        // Lyckas med korrekta datum
+        $db = connectDb();
+        $stmt = $db->query("SELECT YEAR(datum), MONTH(datum), COUNT(*) AS antal "
+            . "FROM uppgifter "
+            . "GROUP BY YEAR(datum), MONTH(datum) "
+            . "ORDER BY antal DESC "
+            . "LIMIT 0,1");
+        $row=$stmt->fetch();
+        $ar = $row[0];
+        $manad = substr("0$row[1]", -2);
+        $antal = $row[2];
+
+        // Hämta alla poster från den funna månaden
+
+        $svar = hamtaSammanstallning("$ar-$manad-01", date("Y-m-d", strtotime("Last day of $ar-$manad")));
+        if ($svar->getStatus() === 200 && count($svar->getContent()->tasks) > 0) {
+            $antal = count($svar->getContent()->tasks);
+            $retur .= "<p class='ok'>Lyckades med att hämta $antal poster mellan $ar-$manad-01</p>";
+        } else {
+            $retur .= "<p class='error'>Misslyckat test med att hämta poster mellan $ar-$manad-01 och " . date("Y-m-d", strtotime("Last day of $ar-$manad")) . "<br>"
+                    . $svar->getStatus() . " Returnerades istället för förväntat 200<br>"
+                        . print_r($svar->getContent()->tasks, true) . "</p>";
+        }
+
+
     } catch (Exception $ex) {
         $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
     }
